b21a4e8b1313c0aa90d73b8a70fe2e71c0662d57
codejlay <jlay@outlook.com>
2015-11-02 17:02:51 -0800

    added product page

---
+++ wp-content/plugins/lrb-functionality/lib/functions/post-types.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * POST TYPES
+ *
+ * @link  http://codex.wordpress.org/Function_Reference/register_post_type
+ */
+
+// Add your custom post types here...
+
+// Register Custom Post Type
+function lrb_cpt_products() {
+
+	$labels = array(
+		'name'                => _x( 'Products', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Product', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Products', 'text_domain' ),
+		'name_admin_bar'      => __( 'Products', 'text_domain' ),
+		'parent_item_colon'   => __( 'Parent Product:', 'text_domain' ),
+		'all_items'           => __( 'All Products', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Product', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'new_item'            => __( 'New Product', 'text_domain' ),
+		'edit_item'           => __( 'Edit Product', 'text_domain' ),
+		'update_item'         => __( 'Update Product', 'text_domain' ),
+		'view_item'           => __( 'View Product', 'text_domain' ),
+		'search_items'        => __( 'Search Product', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$args = array(
+		'label'               => __( 'Product', 'text_domain' ),
+		'description'         => __( 'Create a new product', 'text_domain' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'menu_position'       => 5,
+		'show_in_admin_bar'   => true,
+		'show_in_nav_menus'   => true,
+		'can_export'          => true,
+		'has_archive'         => true,		
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page',
+	);
+	register_post_type( 'product', $args );
+
+}
+add_action( 'init', 'lrb_cpt_products', 0 );
---
+++ wp-content/themes/leredbread/front-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+<?php get_header(); ?>
+
+<div class="site-content">
+	<div class="content-area">
+	<div class="hero">
+		<span class="hero-text">Baked to Perfection.</span>
+	</div>
+	</div>
+</div>
+
+
+<?php get_footer(); ?>
